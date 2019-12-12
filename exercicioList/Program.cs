ccbae4a39dfa431d017e7a4c29fb036d135bb91b
joao Paulo <zheph.jp@gmail.com>
2019-12-12 16:16:25 -0300

    teste aprendizado Lista

---
+++ exercicioList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace exercicioList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("quantos empregados serao registrados?");
+			int employee = int.Parse(Console.ReadLine());
+
+			List<Empregado> emp = new List<Empregado>();
+
+			for (int i = 1; i <= employee; i++)
+			{
+				Console.Write("Id");
+				int id = int.Parse(Console.ReadLine());
+				Console.Write("Nome: ");
+				string nome = Console.ReadLine();
+				Console.WriteLine("Salario : ");
+				double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+				emp.Add(new Empregado(id, nome, salario));
+			}
+			Console.WriteLine("entre com a id do funcionario que tera um aumento de salario : ");
+			int aumento = int.Parse(Console.ReadLine());
+
+			Empregado search = emp.Find(x => x.Id == aumento);
+			emp = null;
+			if(search != null)
+			{
+				Console.WriteLine("entre com o valor do aumento");
+				int porc = int.Parse(Console.ReadLine());
+
+				foreach(Empregado obj in emp)
+				{
+					Console.WriteLine("lista atualizada de emmpregados: ");
+					Console.WriteLine(obj);
+				}
+			}
+			else
+			{
+				foreach (Empregado obj in emp)
+				{
+					Console.WriteLine("lista atualizada de emmpregados: ");
+					Console.WriteLine(obj);
+				}
+			}
+		}
+	}
+}
---
+++ exercicioList/empregado.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace exercicioList
+{
+	class Empregado
+	{
+		public int Id { get; private set; }
+		public string Nome { get; private set; }
+		public double Salario { get; private set; }
+
+		public Empregado(int id, string nome, double salario)
+		{
+			Id = id;
+			Nome = nome;
+			Salario = salario;
+		}
+
+		public  void IncrementoSalario(double porcentagem)
+		{
+			porcentagem = Salario * porcentagem / 100;
+		}
+
+		public override string ToString()
+		{
+			return Id + ", " + Nome + "," + Salario.ToString("f2", CultureInfo.InvariantCulture);
+		}
+	}
+}
